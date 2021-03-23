6eeeb23a8047ff3cc724a26617067d74a364e073
Hamza Outdoors <hamzaoutdoors@gmail.com>
2021-03-23 21:45:27 +0100

    hey you

--- app.js
+++ app.js
@@ -91,15 +91,9 @@ switch(userChoice + computerChoice){
 
   function main(){
 
-    rock_div.addEventListener('click', function(){
-     game ("r") ;
-    })
-    paper_div.addEventListener('click', function(){
-      game ("p") ;
-    })
-    scissors_div.addEventListener('click', function(){
-      game ("s") ;;
-    })
+    rock_div.addEventListener('click', () => game ("r"));
+    paper_div.addEventListener('click', () => game ("p"));
+    scissors_div.addEventListener('click', () => game ("s"));
   }
 
 main();
